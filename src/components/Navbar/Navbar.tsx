b82cf27a435220a0acbe95eb7e00e4ce58fe874e
St3ve89 <istvan.acs89@gmail.com>
2023-12-23 15:31:07 +0000

    Create navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <header className="bg-[#212121] border-b-5 border-[#111]">
+    <header className="sticky top-0 z-10 bg-[#003366] border-b-5 border-[#111]">
       <nav className="flex flex-row flex-wrap justify-between items-center gap-1 p-6 md:px-10 lg:px-40">
         <div className="flex justify-center items-center">
           <Link href="/" aria-label="Go to Home">
@@ -20,6 +20,4 @@ const Navbar = () => {
       </nav>
     </header>
   );
-};
-
-export default Navbar;
+}

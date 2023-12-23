916ea4da19c9b789441c1d51e0f5be95b2c058d6
St3ve89 <istvan.acs89@gmail.com>
2023-12-23 17:29:30 +0000

    Fix navbar link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <header className="sticky top-0 z-10 bg-[#003366] border-b-5 border-[#111]">
       <nav className="flex flex-row flex-wrap justify-between items-center gap-1 p-6 md:px-10 lg:px-40">
         <div className="flex justify-center items-center">
-          <Link href="/" aria-label="Go to Home">
+          <a href="/" aria-label="Go to Home">
             <div className="flex justify-center items-end border-b-2 border-transparent hover:border-[#fbc418] transition duration-400">
               <Image
                 src="/images/pokedex.png"
@@ -15,7 +15,7 @@ export default function Navbar() {
                 alt="Pokédex logo"
               />
             </div>
-          </Link>
+          </a>
         </div>
       </nav>
     </header>

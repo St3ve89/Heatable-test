528e7451d90b7e1f61b105c6caf1f2f5bff63f95
St3ve89 <istvan.acs89@gmail.com>
2023-12-23 15:31:27 +0000

    Create loading component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Loader() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Image
+        src={'/images/loading.gif'}
+        width={300}
+        height={200}
+        alt="Pikachu loading"
+        priority
+      />
+      <h3>Loading...</h3>
+    </div>
+  );
+}

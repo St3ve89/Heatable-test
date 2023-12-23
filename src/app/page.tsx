2f2621d41d9e053452337ba4e42a3df0dc5285e2
St3ve89 <istvan.acs89@gmail.com>
2023-12-23 15:48:02 +0000

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import LoadMore from '@/components/LoadMore/LoadMore';
+import Loader from '@/components/Loader/Loader';
+import PokemonList from '@/components/PokemonList/PokemonList';
+import SearchBar from '@/components/SearchBar/SearchBar';
 import localFont from 'next/font/local';
+import { Suspense } from 'react';
 
 const pokemonFont = localFont({
   src: 'fonts/Pokemon Solid.ttf',
 });
 
-export default function Home() {
+interface HomeProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const search =
+    typeof searchParams.search === 'string' ? searchParams.search : undefined;
+
   return (
     <div>
       <div className="flex justify-center items-start p-8 bg-[url('/images/bg-title.png')] bg-contain bg-center bg-repeat-x sm:p-4">
@@ -14,7 +26,15 @@ export default function Home() {
           Choose your Pokémon
         </h1>
       </div>
-      hello
+      <div className="p-6 md:px-10 lg:px-40">
+        <div className="flex flex-col items-center space-y-4 max-w-max mx-auto">
+          <SearchBar search={search} />
+          <Suspense fallback={<Loader />}>
+            <PokemonList query={search} />
+          </Suspense>
+          {!search && <LoadMore />}
+        </div>
+      </div>
     </div>
   );
 }
